a23befefa02fff5967187c955b9e4c996b75cb7a
곽경진 <kgj1212_@naver.com>
2021-03-05 21:47:12 +0900

    [feat] axios data innerHTML

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,5 +1,5 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = 'https://jsonplaceholder.typicode.com/users';
 
 // dom
 var username = document.querySelector('#username');
@@ -16,6 +16,10 @@ function startApp() {
       console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user)
+      username.innerHTML = user[0].name;
+      email.innerHTML = user[0].email;
+      address.innerHTML = user[0].address.street;
     })
     .catch(function (error) {
       console.log(error);
