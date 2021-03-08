ac234563a4f064818499c508f51a6f9ae1e03e29
곽경진 <kgj1212_@naver.com>
2021-03-08 20:43:31 +0900

    [feat] add jsdock 사전 에러 방지 방법

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,29 +1,50 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users';
+var url = 'https://jsonplaceholder.typicode.com/users/1'
 
 // dom
-var username = document.querySelector('#username');
-var email = document.querySelector('#email');
-var address = document.querySelector('#address');
+var username = document.querySelector('#username')
+var email = document.querySelector('#email')
+var address = document.querySelector('#address')
 
 // user data
-var user = {};
+var user = {}
 
-function startApp() {
-  axios
-    .get(url)
-    .then(function (response) {
-      console.log(response);
-      user = response.data;
-      // TODO: 이름, 이메일, 주소 표시하기
-      console.log(user)
-      username.innerHTML = user[0].name;
-      email.innerHTML = user[0].email;
-      address.innerHTML = user[0].address.street;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {string}
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url)
 }
 
-startApp();
+// fetchUser().then(function(response){
+//   response.email
+// })
+
+// console.log(fetchUser())
+
+function startApp() {
+  // axios
+  //   .get(url)
+  fetchUser()
+    .then(function (response) {
+      console.log(response)
+      user = response.data
+      // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user)
+      username.innerHTML = user.name
+      email.innerHTML = user.email
+      address.innerHTML = user.address.street
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+}
+
+startApp()
